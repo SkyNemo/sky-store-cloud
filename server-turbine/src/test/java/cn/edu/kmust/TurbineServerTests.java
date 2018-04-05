f4a518c63d63d021dbc35b8eb514eae45aff6ae3
nemo <sky.nemo@outlook.com>
2018-04-05 21:29:01 +0800

    server turbine for monitor microservice

---
+++ server-turbine/src/test/java/cn/edu/kmust/TurbineServerTests.java
@@ -0,0 +1,31 @@
+package cn.edu.kmust;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TurbineServerTests {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(TurbineServerTests.class);
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Value("${server.port}")
+	public String port;
+
+
+	@Test
+	public void testConfig(){
+
+		LOGGER.info("server.port = " + port);
+	}
+
+}
